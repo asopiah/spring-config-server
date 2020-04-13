5b8411cedc03faba5d68f4e132d681ad29e63080
Alfred Sopia <asopia@strathmore.edu>
2020-04-13 15:47:21 +0300

    Initial commit with catelog-service config properties

---
+++ src/test/java/com/sopia/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+/*package com.sopia.configserver;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Ignore
+class ConfigServerApplicationTests {
+
+	@Test
+	@Ignore
+	void contextLoads() {
+	}
+
+}*/
